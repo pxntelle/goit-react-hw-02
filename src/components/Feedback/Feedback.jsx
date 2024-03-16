81903388c4c93618d132419314f9ae877913f643
pxntelle <taniapantelle@gmail.com>
2024-03-16 02:00:50 +0100

    extra check deleted

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,17 +4,11 @@ import Notification from "../Notification/Notification";
 export default function Feedback({ clicks, totalFeedback, positiveRate }) {
   return (
     <div className={css.fbSection}>
-      {totalFeedback === 0 ? (
-        <Notification />
-      ) : (
-        <>
-          <p>Good: {clicks.good}</p>
-          <p>Neutral: {clicks.neutral}</p>
-          <p>Bad: {clicks.bad}</p>
-          <p>Total: {totalFeedback}</p>
-          <p>Positive: {positiveRate}%</p>
-        </>
-      )}
+      <p>Good: {clicks.good}</p>
+      <p>Neutral: {clicks.neutral}</p>
+      <p>Bad: {clicks.bad}</p>
+      <p>Total: {totalFeedback}</p>
+      <p>Positive: {positiveRate}%</p>
     </div>
   );
 }
